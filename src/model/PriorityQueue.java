15bb01b8d492b83517fc76c7e64357a43d56f1a3
joshuakuriakose <jkuri749@mtroyal.ca>
2022-10-30 01:21:18 -0600

    hgj

--- src/model/PriorityQueue.java
+++ src/model/PriorityQueue.java
@@ -7,6 +7,26 @@ public class PriorityQueue<T> implements QueueInterface<T>{
 	@Override
 	public void enqueue(T t) {
 		
+//		QNode<T> newNode = new QNode<T>(t);  
+//		   
+//	    //if list is empty, head and tail points to newNode  
+//	    if(isEmpty()) {  
+//	    	front = rear = newNode;  
+//	            //start's previous will be null  
+//	    	front.setPrev(null);  
+//	            //end's next will be null  
+//	    	rear.setNext(null);  
+//	    }  
+//	    else {  
+//	            //add newNode to the end of list. end->next set to newNode  
+//	        rear.setNext(newNode);  
+//	            //newNode->previous set to end  
+//	        newNode.setPrev(rear);  
+//	            //newNode becomes end 
+//	        rear = newNode;  
+//	            //end's next point to null  
+//	        rear.setNext(null);  
+//	    }  
 	}
 	@Override
 	public T dequeue() {
