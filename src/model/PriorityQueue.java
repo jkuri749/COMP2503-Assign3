409824ede6ffde658652769d26ea286c28068d1e
joshuakuriakose <jkuri749@mtroyal.ca>
2022-10-30 01:09:09 -0600

    Priority Queue started

---
+++ src/model/PriorityQueue.java
@@ -0,0 +1,31 @@
+package model;
+
+public class PriorityQueue<T> implements QueueInterface<T>{
+	private int size;
+	private QNode<T> front;
+	private QNode<T> rear;
+	@Override
+	public void enqueue(T t) {
+		
+	}
+	@Override
+	public T dequeue() {
+		return null;
+	}
+	@Override
+	public T peek() {
+		return null;
+	}
+	@Override
+	public int getSize() {
+		return 0;
+	}
+	@Override
+	public boolean isEmpty() {
+		return false;
+	}
+	@Override
+	public void printQueue() {
+		
+	}
+}
