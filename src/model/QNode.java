ebe587cd68aba866519ea5f6721cd9dd1e2e8a88
gurmangrewal <gurmangrewal@192.168.1.69>
2022-10-30 16:52:26 -0600

    QNode complete

--- src/model/QNode.java
+++ src/model/QNode.java
@@ -1,5 +1,31 @@
 package model;
 
 public class QNode<T> {
-
+	
+	private T data;
+	private QNode<T> next;
+	
+	public QNode() {
+		data=null;
+	}
+	
+	public QNode(T data) {
+		this.data=data;
+	}
+	
+	public T getData() {
+		return data;
+	}
+	
+	public void setData(T value) {
+		data=value;
+	}
+	
+	public void setNext(QNode<T> other) {
+		next=other;
+	}
+	
+	public QNode<T> getNext(){
+		return next;
+	}
 }
