5a4a74ff533af40106a0f9d4ebe6d9062ba7a23a
gurmangrewal <gurmangrewal@192.168.1.69>
2022-11-01 23:54:40 -0600

    Merge branch 'main' of https://github.com/jkuri749/COMP2503-Assign3 into main

---
+++ src/test/LinkedQueueTest.java
@@ -0,0 +1,55 @@
+package test;
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.NoSuchElementException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import model.LinkedQueue;
+
+public class LinkedQueueTest<T> {
+	
+	LinkedQueue<T> queue;
+	
+	@Test
+	@DisplayName("is instantiated with new LinkedListQueue()")
+	void isInstantiatedWithNew() {
+		new LinkedQueue<T>();
+	}
+	
+	@BeforeEach
+	void createNewQueue() {
+		queue = new LinkedQueue<T>();
+	}
+	
+	@Test
+	@DisplayName("is empty")
+	void isEmpty() {
+		assertTrue(queue.isEmpty());
+	}
+	
+	@Test
+	@DisplayName("throws NoSuchElementException when enqueued")
+	void throwsExceptionWhenEnqueued() {
+		assertThrows(NoSuchElementException.class, queue::enqueue);
+	}
+	
+	@Test
+	@DisplayName("throws NoSuchElementException when peeked")
+	void throwsExceptionWhenPeeked() {
+		assertThrows(NoSuchElementException.class, queue::peek);
+	}
+	
+	@Nested
+	@DisplayName("after enqueueing an object")
+	class AfterEnqueue{
+		
+		Queue aQueue = new 
+	}
+
+}
